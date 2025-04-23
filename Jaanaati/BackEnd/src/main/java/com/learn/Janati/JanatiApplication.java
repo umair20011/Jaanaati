c60cdb6ce0dd76253f1533b629835fe472e6a868
Danish <danishmaniyar2110@gmail.com>
2025-04-23 14:59:22 +0530

    generated backend webapp

---
+++ Jaanaati/BackEnd/src/main/java/com/learn/Janati/JanatiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.Janati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanatiApplication.class, args);
+	}
+
+}
---
+++ Jaanaati/BackEnd/src/test/java/com/learn/Janati/JanatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Janati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
